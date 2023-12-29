bc9fd85e7f332df4134daf35ffe13b6c510593a7
Prohack101010 <126958051+Prohack101010@users.noreply.github.com>
2023-12-30 02:54:28 +0300

    Update CustomSubstate.hx

--- source/psychlua/CustomSubstate.hx
+++ source/psychlua/CustomSubstate.hx
@@ -6,13 +6,14 @@ class CustomSubstate extends MusicBeatSubstate
 {
 	public static var name:String = 'unnamed';
 	public static var instance:CustomSubstate;
-	
+
 	public static function implement(funk:FunkinLua)
-		{
-                #if LUA_ALLOWED
-		funk.set("openCustomSubstate", openCustomSubstate);
-		funk.set("closeCustomSubstate", closeCustomSubstate);
-		funk.set("insertToCustomSubstate", insertToCustomSubstate);
+	{
+		#if LUA_ALLOWED
+		var lua = funk.lua;
+		Lua_helper.add_callback(lua, "openCustomSubstate", openCustomSubstate);
+		Lua_helper.add_callback(lua, "closeCustomSubstate", closeCustomSubstate);
+		Lua_helper.add_callback(lua, "insertToCustomSubstate", insertToCustomSubstate);
 		#end
 	}
 	
